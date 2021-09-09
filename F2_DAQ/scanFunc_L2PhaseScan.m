dc135cf2a2ec6f56298058d2275b8f7749946022
SLAC fphysics <cemma@slac.stanford.edu>
2021-09-09 10:43:23 -0700

    added L2 Phase Scan Func (still work in progress)

---
+++ F2_DAQ/scanFunc_L2PhaseScan.m
@@ -0,0 +1,80 @@
+classdef scanFunc_L2PhaseScan
+    properties
+        pvlist PV
+        pvs
+        initial_control
+        initial_readback
+        daqhandle
+        freerun = true
+    end
+    properties(Constant)
+        control_PV = "SIOC:SYS1:ML02:AO399"
+        readback_PV = "SIOC:SYS1:ML02:AO399"
+        tolerance = 0.01;
+    end
+    
+    methods 
+        
+        function obj = scanFunc_Dummy_Scan(daqhandle)
+            
+            % Check if scanfunc called by DAQ
+            if exist('daqhandle','var')
+                obj.daqhandle=daqhandle;
+                obj.freerun = false;
+            end
+        
+            % store scan range
+%             handles.data.range = linspace(-diff(prange)/2, diff(prange)/2, handles.data.nstep);
+%             rangestr = '';
+%             for ix = 1:handles.data.nstep
+%                 rangestr = [rangestr sprintf('%.1f ', handles.data.range(ix))];
+%             end
+%             gui_statusDisp(handles, sprintf('Calculated knob steps: [%s]', rangestr));
+% 
+%             phase_deltas = diff([0 handles.data.range 0]);
+
+            % set up AIDA for knob control
+%             aidainit;
+%             import edu.stanford.slac.aida.lib.da.DaObject;
+%             da = DaObject();
+%             da.setParam('MKB', strcat('mkb:', handles.data.knob));
+            
+            
+            context = PV.Initialize(PVtype.EPICS_labca);
+            obj.pvlist=[...
+                PV(context,'name',"control",'pvname',obj.control_PV,'mode',"rw",'monitor',true); % Control PV
+                PV(context,'name',"readback",'pvname',obj.readback_PV,'mode',"r",'monitor',true); % Readback PV
+                ];
+            pset(obj.pvlist,'debug',0);
+            obj.pvs = struct(obj.pvlist);
+            
+            obj.initial_control = caget(obj.pvs.control);
+            obj.initial_readback = caget(obj.pvs.readback);
+            
+        end
+        
+        function delta = set_value(obj,value)
+            
+            caput(obj.pvs.control,value);
+            obj.daqhandle.dispMessage(sprintf('Setting %s to %0.2f', obj.pvs.control.name, value));
+            
+            current_value = caget(obj.pvs.readback);
+            
+            while abs(current_value - value) > obj.tolerance
+                current_value = caget(obj.pvs.readback);
+                pause(0.1);
+            end
+            
+            delta = current_value - value;
+            obj.daqhandle.dispMessage(sprintf('%s readback is %0.2f', obj.pvs.readback.name, current_value));
+            
+        end
+        
+        function restoreInitValue(obj)
+            obj.daqhandle.dispMessage('Restoring initial value');
+            obj.set_value(obj.initial_control);
+        end
+        
+    end
+    
+end
