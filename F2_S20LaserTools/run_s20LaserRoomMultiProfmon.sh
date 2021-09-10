f43a4ab3ca3b27e05b491b55aebf9300f73f80ce
SLAC fphysics <cemma@slac.stanford.edu>
2021-09-09 17:21:38 -0700

    fix path of s20laserroom multiprofmon

--- F2_S20LaserTools/run_s20LaserRoomMultiProfmon.sh
+++ F2_S20LaserTools/run_s20LaserRoomMultiProfmon.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-#source /usr/local/lcls/epics/setup/epicsenv-7.0.2-1.0.bash
-#source /usr/local/lcls/tools/oracle/oracleSetup-R11.2.0.4.bash
-#export MATLAB_VER=2019a
-#source /usr/local/lcls/tools/matlab/setup/matlabSetup64.bash
-run_matlab.bash -m 2020a -r "addpath('/home/fphysics/cemma/S20Laser/S20LaserMultiProfmonGUI'); run('/home/fphysics/cemma/S20Laser/S20LaserMultiProfmonGUI/s20LaserRoomMultiProfmon')"
+run_matlab.bash -m 2020a -r "addpath('/usr/local/facet/tools/matlabTNG/F2_S20LaserTools'); run('/usr/local/facet/tools/matlabTNG/F2_S20LaserTools/s20LaserRoomMultiProfmon')"
