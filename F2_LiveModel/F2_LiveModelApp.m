63ec03d81cb29448a04f4fb15957bfd4f3d9caa1
SLAC fphysics <whitegr@slac.stanford.edu>
2021-10-08 16:16:35 -0700

    Add LiveModel and BC20 tuning GUIs

--- F2_LiveModel/F2_LiveModelApp.m
+++ F2_LiveModel/F2_LiveModelApp.m
@@ -2,17 +2,19 @@ classdef F2_LiveModelApp < handle & F2_common
   properties
     LEM % LEM App object
     LM % Lucretia Model object
+    Initial % Lucretia initial structure for start of lattice
   end
   properties(SetAccess=private)
     DesignTwiss
     DesignInitial
+    pvs
+    pvlist
   end
   properties(Access=private)
     InjSolInd
     InjSolBkInd
   end
   properties(SetObservable)
-    Initial % Lucretia initial structure for start of lattice
     ModelSource string {mustBeMember(ModelSource,["Live" "Archive" "Design"])} = "Live"
   end
   properties(Constant)
@@ -26,17 +28,32 @@ classdef F2_LiveModelApp < handle & F2_common
   methods
     function obj = F2_LiveModelApp
       global BEAMLINE
+      
       % Get data from design model
       obj.LM=LucretiaModel;
       [~,obj.DesignTwiss] = GetTwiss(1,length(BEAMLINE),obj.LM.Initial.x.Twiss,obj.LM.Initial.y.Twiss) ;
       obj.DesignInitial = obj.LM.Initial ;
+      
+      % PV lists
+      cntx=PV.Initialize(PVtype.EPICS);
+      obj.pvlist=[PV(cntx,'name',"betax",'pvname',obj.Initial_betaxPV);
+                  PV(cntx,'name',"betay",'pvname',obj.Initial_betayPV);
+                  PV(cntx,'name',"alphax",'pvname',obj.Initial_alphaxPV);
+                  PV(cntx,'name',"alphay",'pvname',obj.Initial_alphayPV)
+                  PV(cntx,'name',"emitx",'pvname',obj.Initial_emitxPV)
+                  PV(cntx,'name',"emity",'pvname',obj.Initial_emityPV)] ;
+      pset(obj.pvlist,'debug',0) ;
+      obj.pvs = struct(obj.pvlist) ;
+      
+      % Get Initial Twiss parameters from last injector emittance scan
       obj.Initial = obj.LM.Initial;
-      obj.Initial.x.NEmit = lcaGet(char(obj.Initial_emitxPV)).*1e-6 ;
-      obj.Initial.y.NEmit = lcaGet(char(obj.Initial_emityPV)).*1e-6 ;
-      obj.Initial.x.beta = lcaGet(char(obj.Initial_betaxPV)) ;
-      obj.Initial.y.beta = lcaGet(char(obj.Initial_betayPV)) ;
-      obj.Initial.x.alpha = lcaGet(char(obj.Initial_alphaxPV)) ;
-      obj.Initial.y.alpha = lcaGet(char(obj.Initial_alphayPV)) ;
+      obj.Initial.x.NEmit = caget(obj.pvs.emitx).*1e-6 ;
+      obj.Initial.y.NEmit = caget(obj.pvs.emity).*1e-6 ;
+      obj.Initial.x.beta = caget(obj.pvs.betax) ;
+      obj.Initial.y.beta = caget(obj.pvs.betay) ;
+      obj.Initial.x.alpha = caget(obj.pvs.alphax) ;
+      obj.Initial.y.alpha = caget(obj.pvs.alphay) ;
+      
       % Make live model
       addpath('../F2_LEM');
       if obj.ModelSource~="Design"
@@ -53,6 +70,14 @@ classdef F2_LiveModelApp < handle & F2_common
         case "Archive"
           obj.UseArchive=true;
           obj.LEM.UseArchive=true;
+          % Load Initial structure from archived injector emittance scan data
+          pset(obj.pvlist,'ArchiveDate',obj.ArchiveDate) ;
+          obj.Initial.x.NEmit = caget(obj.pvs.emitx).*1e-6 ;
+          obj.Initial.y.NEmit = caget(obj.pvs.emity).*1e-6 ;
+          obj.Initial.x.beta = caget(obj.pvs.betax) ;
+          obj.Initial.y.beta = caget(obj.pvs.betay) ;
+          obj.Initial.x.alpha = caget(obj.pvs.alphax) ;
+          obj.Initial.y.alpha = caget(obj.pvs.alphay) ;
         case "Design"
           obj.UseArchive=false;
           obj.LEM.UseArchive=false;
@@ -60,16 +85,16 @@ classdef F2_LiveModelApp < handle & F2_common
       end
       obj.ModelSource=src;
     end
-    function set.Initial(obj,Inew)
-      global BEAMLINE
-      [stat,Twiss] = GetTwiss(1,length(BEAMLINE),Inew.x.Twiss,Inew.y.Twiss) ;
-      if stat{1}~=1
-        error('Invalid Initial structure')
-      else
-        obj.DesignTwiss = Twiss;
-        obj.Initial = Inew ;
-      end
-    end
+%     function set.Initial(obj,Inew)
+%       global BEAMLINE
+%       [stat,Twiss] = GetTwiss(1,length(BEAMLINE),Inew.x.Twiss,Inew.y.Twiss) ;
+%       if stat{1}~=1
+%         error('Invalid Initial structure')
+%       else
+%         obj.DesignTwiss = Twiss;
+%         obj.Initial = Inew ;
+%       end
+%     end
     function UpdateModel(obj)
       fprintf('Updating live model...');
       % Sync archiver settings
@@ -84,7 +109,17 @@ classdef F2_LiveModelApp < handle & F2_common
       if ~exist('fname','var') || isempty(fname)
         fname = obj.confdir+"/F2_LiveModel/LiveModel.mat" ;
       end
-      save(fname,'LEM','BEAMLINE','PS','GIRDER','WF','KLYSTRON');
+      Initial=obj.Initial; %#ok<PROPLC>
+      save(fname,'LEM','BEAMLINE','PS','GIRDER','WF','KLYSTRON','Initial');
+    end
+    function LoadModel(obj,fname)
+      global BEAMLINE PS GIRDER WF KLYSTRON
+      if ~exist('fname','var') || isempty(fname)
+        fname = obj.confdir+"/F2_LiveModel/LiveModel.mat" ;
+      end
+      load(fname,'LEM','BEAMLINE','PS','GIRDER','WF','KLYSTRON','Initial');
+      obj.LEM=LEM; %#ok<PROPLC>
+      obj.Initial=Initial; %#ok<PROPLC>
     end
   end
 end
