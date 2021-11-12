761ba3c406e70984a89f9a4941344494e713ec25
SLAC fphysics <whitegr@slac.stanford.edu>
2021-11-11 18:49:13 -0800

    fix applauncher script

--- applauncher.sh
+++ applauncher.sh
@@ -3,4 +3,4 @@
 #ps -eF | grep "Matlab AppLauncher"
 MATCMD="/usr/local/lcls/package/matlab/2020a/bin/matlab"
 MATDIR="/usr/local/facet/tools/matlabTNG"
-xterm -iconic -T "Matlab AppLauncher xterm" -e bash -ilc "cd $MATDIR; $MATCMD -nodesktop -nosplash -r applauncher" &
+xterm -iconic -T "Matlab AppLauncher xterm" -e "cd $MATDIR; $MATCMD -nodesktop -nosplash -r applauncher" &
