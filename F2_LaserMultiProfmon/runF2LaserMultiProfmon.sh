41c75fc5fee98a61776abf733d00f3c79255c5d2
SLAC fphysics <whitegr@slac.stanford.edu>
2021-11-17 10:35:09 -0800

    Change watchdog PVs

--- F2_LaserMultiProfmon/runF2LaserMultiProfmon.sh
+++ F2_LaserMultiProfmon/runF2LaserMultiProfmon.sh
@@ -3,5 +3,6 @@
 #source /usr/local/lcls/oracle/oracleSetup-R11.2.0.4.bash
 #export MATLAB_VER=2019_a
 #source /usr/local/lcls/tools/matlab/setup/matlabSetup64.bash
+cd /usr/local/facet/tools/matlabTNG/F2_LaserMultiProfmon
 run_matlab.bash -m 2020a  -r "runF2LaserMultiProfmon('$1');"
 
