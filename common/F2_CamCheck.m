566e06940e6f60d3b7006347f9e4f9c8048cbc79
SLAC fphysics <sgess@slac.stanford.edu>
2022-05-01 21:49:38 -0700

    fixed IOC for S10 cams

--- common/F2_CamCheck.m
+++ common/F2_CamCheck.m
@@ -95,9 +95,9 @@ classdef F2_CamCheck < handle
         function add_SIOCs(obj)
             
             obj.sioc_list = {'cpu-lr10-pm01',       'SIOC:LR10:PM01';
-                             'cpu-in10-pm01',       'SIOC:IN10:PM02';
+                             'cpu-in10-pm01',       'SIOC:IN10:PM01';
                              %'cpu-in10-ls01',       'SIOC:IN10:LS01';
-                             'cpu-li10-pm01',       'SIOC:LI10:PM01';
+                             'cpu-li10-pm01',       'SIOC:LI10:PM02';
                              'cpu-li14-pm01',       'SIOC:LI14:PM01';
                              'cpu-li15-pm01',       'SIOC:LI15:PM01';
                              'cpu-li20-pm01',       'SIOC:LI20:PM01';
