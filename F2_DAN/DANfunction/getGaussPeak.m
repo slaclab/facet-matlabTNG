1150d38373dba2e18c122608d0c7c77ffd0669ee
SLAC fphysics <henrik01@slac.stanford.edu>
2022-08-01 18:26:32 -0700

    Add analysis functions for DAN

---
+++ F2_DAN/DANfunction/getGaussPeak.m
@@ -0,0 +1,16 @@
+function peakIdx = getGaussPeak(x,sumDir)
+% peakIdx = GETPEAK(x,sumDir)
+% x is the 2D matrix to get projected peak from, sumDir is the dimension to
+% sum over (1 (columns) or 2 (rows))
+    
+    if nargin == 1
+        sumDir = 1
+    end
+    
+    vec = sum(x,sumDir);
+
+    %find last peak
+    [yfit,q,dq,chisq_ndf] = gauss_fit(1:length(vec),vec);
+    peakIdx = q(3);
+%     peakIdx = find(max(vec)==vec,1,'last');
+end
