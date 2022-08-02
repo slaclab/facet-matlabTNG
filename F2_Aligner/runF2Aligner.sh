37a860c7e710bf2d36f36adfa428a4afe544df35
SLAC fphysics <henrik01@slac.stanford.edu>
2022-08-01 18:48:15 -0700

    Add start script for F2_Aligner

---
+++ F2_Aligner/runF2Aligner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#source /usr/local/lcls/epics/setup/epicsenv-7.0.2-1.0.bash
+#source /usr/local/lcls/oracle/oracleSetup-R11.2.0.4.bash
+#export MATLAB_VER=2019_a
+#source /usr/local/lcls/tools/matlab/setup/matlabSetup64.bash
+run_matlab.bash -m 2020a  -r "F2_Aligner_exported"
+
