e139285029affe8787df52c2ea3a82bec928f93a
sebastian meuren <smeuren@stanford.edu>
2022-08-12 23:22:31 -0700

    added scan function for E-320 YAG target horizontal picomotor

---
+++ F2_DAQ/scanFunc_E320_YAG_horizontal.m
@@ -0,0 +1,63 @@
+classdef scanFunc_E320_YAG_horizontal
+    properties
+        pvlist PV
+        pvs
+        initial_control
+        initial_readback
+        daqhandle
+        freerun = true
+    end
+    properties(Constant)
+        control_PV = "MOTR:LI20:MC08:S6:CH2:MOTOR"
+        readback_PV = "MOTR:LI20:MC08:S6:CH2:MOTOR.RBV"
+        tolerance = 0.01;
+    end
+    
+    methods 
+        
+        function obj = scanFunc_E320_YAG_horizontal(daqhandle)
+            
+            % Check if scanfunc called by DAQ
+            if exist('daqhandle','var')
+                obj.daqhandle=daqhandle;
+                obj.freerun = false;
+            end
+                    
+            context = PV.Initialize(PVtype.EPICS_labca);
+            obj.pvlist=[...
+                PV(context,'name',"control",'pvname',obj.control_PV,'mode',"rw",'monitor',true); % Control PV
+                PV(context,'name',"readback",'pvname',obj.readback_PV,'mode',"r",'monitor',true); % Readback PV
+                ];
+            pset(obj.pvlist,'debug',0);
+            obj.pvs = struct(obj.pvlist);
+            
+            obj.initial_control = caget(obj.pvs.control);
+            obj.initial_readback = caget(obj.pvs.readback);
+            
+        end
+        
+        function delta = set_value(obj,value)
+            
+            caput(obj.pvs.control,value);
+            obj.daqhandle.dispMessage(sprintf('Setting %s to %0.3f', obj.pvs.control.name, value));
+            
+            current_value = caget(obj.pvs.readback);
+            
+            while abs(current_value - value) > obj.tolerance
+                current_value = caget(obj.pvs.readback);
+                pause(0.1);
+            end
+            
+            delta = current_value - value;
+            obj.daqhandle.dispMessage(sprintf('%s readback is %0.3f', obj.pvs.readback.name, current_value));
+            
+        end
+        
+        function restoreInitValue(obj)
+            obj.daqhandle.dispMessage('Restoring initial value');
+            obj.set_value(obj.initial_control);
+        end
+        
+    end
+    
+end
