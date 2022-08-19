b3f5b75e773d29ba0e699592affcd9b9e822147d
Facet Physics Account <fphysics@opi23.slac.stanford.edu>
2022-08-18 18:00:40 -0700

    bugfix applauncher kill commands

--- applauncher_kill.sh
+++ applauncher_kill.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-/usr/local/facet/tools/matlabTNG/applauncher_client SHUTDOWN
+/usr/local/facet/tools/matlabTNG/applauncher_client.sh SHUTDOWN
 wait 1
-/usr/local/facet/tools/matlabTNG/applauncher_client SHUTDOWN
+/usr/local/facet/tools/matlabTNG/applauncher_client.sh SHUTDOWN
 wait 1
-/usr/local/facet/tools/matlabTNG/applauncher_client SHUTDOWN
+/usr/local/facet/tools/matlabTNG/applauncher_client.sh SHUTDOWN
 wait 1
-/usr/local/facet/tools/matlabTNG/applauncher_client SHUTDOWN
+/usr/local/facet/tools/matlabTNG/applauncher_client.sh SHUTDOWN
 wait 1
-/usr/local/facet/tools/matlabTNG/applauncher_client SHUTDOWN
+/usr/local/facet/tools/matlabTNG/applauncher_client.sh SHUTDOWN
