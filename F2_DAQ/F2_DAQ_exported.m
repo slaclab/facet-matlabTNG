6ce9b40806963604bfccd707d6746ce215b6f7e2
SLAC fphysics <sgess@slac.stanford.edu>
2022-10-27 17:31:28 -0700

    added E301

--- F2_DAQ/F2_DAQ_exported.m
+++ F2_DAQ/F2_DAQ_exported.m
@@ -389,7 +389,7 @@ classdef F2_DAQ_exported < matlab.apps.AppBase
 
             % Create ExperimentDropDown
             app.ExperimentDropDown = uidropdown(app.DAQSettingsPanel);
-            app.ExperimentDropDown.Items = {'TEST', 'E300', 'E305', 'E308', 'E320', 'E325', 'E326', 'E327', 'E331', 'E332'};
+            app.ExperimentDropDown.Items = {'TEST', 'E300', 'E301', 'E305', 'E308', 'E320', 'E325', 'E326', 'E327', 'E331', 'E332'};
             app.ExperimentDropDown.Position = [88 187 71 22];
             app.ExperimentDropDown.Value = 'TEST';
 
