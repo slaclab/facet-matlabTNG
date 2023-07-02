9b715868b8e5c513d68f997cdd27f3685a356df2
SLAC fphysics <cemma@slac.stanford.edu>
2023-07-01 22:14:53 -0700

    changed displayLaserCentroidForSingleCamera.m from a script to a function

--- F2_LAA/displayLaserCentroidForSingleCamera.m
+++ F2_LAA/displayLaserCentroidForSingleCamera.m
@@ -1,6 +1,6 @@
-% Display laser centroid on camera
-% Input the camera PV
-cameraPV = 'CAMR:LT20:100';
+function [Xcentroid,Ycentroid] = displayLaserCentroidForSingleCamera(cameraPV)
+% Display laser centroid on camera. The user has to input the camera PV
+% Example:  [Xcentroid,Ycentroid] = displayLaserCentroidForSingleCamera('CAMR:LT20:100');
 
 % Get the laser centroid using the same method as the auto-aligner
 opts = struct('usemethod',2);
@@ -11,11 +11,12 @@ data = profmon_grab(cameraPV,0);
 beamParams = beamAnalysis_beamParams(img, 1:size(img,2), 1:size(img,1),0,opts);
 stats = beamParams.stats;
 
-Xcentroid = stats(1)
-Ycentroid = stats(2)
+Xcentroid = stats(1);
+Ycentroid = stats(2);
 
-figure;imagesc(img);xlabel('X [pix');ylabel('Y [pix]');
+figure;imagesc(img);xlabel('X [pix]');ylabel('Y [pix]');
 hold on;plot(Xcentroid,Ycentroid,'r*','MarkerSize',10);
-str = sprintf(['Xctr = ',num2str(Xcentroid,'%.2f'), ' Yctr = ',num2str(Ycentroid,'%.2f')])
+str = sprintf(['Xctr = ',num2str(Xcentroid,'%.2f'), ' Yctr = ',num2str(Ycentroid,'%.2f')]);
 title(str)
 set(gca,'FontSize',20,'LineWidth',2)
+end
