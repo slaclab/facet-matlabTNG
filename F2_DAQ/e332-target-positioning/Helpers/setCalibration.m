510408efa4bd0c8a19c51668dd8d8b880678c5f1
SLAC fphysics <mgill@slac.stanford.edu>
2023-11-10 23:16:42 -0800

    E332: Changed calibration

--- F2_DAQ/e332-target-positioning/Helpers/setCalibration.m
+++ F2_DAQ/e332-target-positioning/Helpers/setCalibration.m
@@ -5,6 +5,7 @@ pvEngine = PVEngineLca();
 numTargets_PV = "SIOC:SYS1:ML03:AO652";
 pvEngine.put(numTargets_PV, 3)
 
+
 % Target 1
 
 targetNumber = 1;
@@ -13,12 +14,12 @@ targetType = Target_H15_C1_V11();
 calibrationData.targetTypeNumber = TargetDefinition.targetNumberByType(targetType);
 
 calibrationData.hole(1) = targetType.holeNumberFromString("Y4");
-calibrationData.lat(1) = -97.1;
-calibrationData.vert(1) = -3.9;
+calibrationData.lat(1) = -97.3;
+calibrationData.vert(1) = -4;
 
 calibrationData.hole(2) = targetType.holeNumberFromString("C31");
-calibrationData.lat(2) = -61.7;
-calibrationData.vert(2) = -36;
+calibrationData.lat(2) = -62;
+calibrationData.vert(2) = -36.1;
 
 calibration = Calibration(pvEngine);
 calibration.setCalibration(targetNumber, calibrationData);
@@ -32,12 +33,12 @@ targetType = Target_H15_C1_V10();
 calibrationData.targetTypeNumber = TargetDefinition.targetNumberByType(targetType);
 
 calibrationData.hole(1) = targetType.holeNumberFromString("C2");
-calibrationData.lat(1) = -48.85;
+calibrationData.lat(1) = -49.15;
 calibrationData.vert(1) = -35.55;
 
 calibrationData.hole(2) = targetType.holeNumberFromString("Z33");
-calibrationData.lat(2) = -8.75;
-calibrationData.vert(2) = -0.1;
+calibrationData.lat(2) = -8.95;
+calibrationData.vert(2) = -0.2;
 
 calibration = Calibration(pvEngine);
 calibration.setCalibration(targetNumber, calibrationData);
@@ -53,12 +54,12 @@ targetType = Target_H15_C1_V11();
 calibrationData.targetTypeNumber = TargetDefinition.targetNumberByType(targetType);
 
 calibrationData.hole(1) = targetType.holeNumberFromString("X4");
-calibrationData.lat(1) = -77.484;
-calibrationData.vert(1) = -55.42;
+calibrationData.lat(1) = -77.684;
+calibrationData.vert(1) = -55.52;
 
 calibrationData.hole(2) = targetType.holeNumberFromString("B31");
-calibrationData.lat(2) = -41.784;
-calibrationData.vert(2) = -87.02;
+calibrationData.lat(2) = -41.984;
+calibrationData.vert(2) = -87.12;
 
 calibration = Calibration(pvEngine);
 calibration.setCalibration(targetNumber, calibrationData);
