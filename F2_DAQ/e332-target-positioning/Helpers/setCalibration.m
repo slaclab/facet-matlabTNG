bea0ed17bdfd240de192416a8dd1b3aa17eee301
SLAC fphysics <mgill@slac.stanford.edu>
2023-11-10 20:38:43 -0800

    E332: Modified calibration script, fixed bug.

--- F2_DAQ/e332-target-positioning/Helpers/setCalibration.m
+++ F2_DAQ/e332-target-positioning/Helpers/setCalibration.m
@@ -1,5 +1,5 @@
-pvEngine = PVEngineMock();
-pvEngine.verbose = true;
+pvEngine = PVEngineLca();
+%pvEngine.verbose = true;
 
 % Set PV with number of targets
 numTargets_PV = "SIOC:SYS1:ML03:AO652";
@@ -8,17 +8,17 @@ pvEngine.put(numTargets_PV, 3)
 % Target 1
 
 targetNumber = 1;
-targetType = Target_H15_C1_V10();
+targetType = Target_H15_C1_V11();
 
 calibrationData.targetTypeNumber = TargetDefinition.targetNumberByType(targetType);
 
-calibrationData.hole(1) = targetType.holeNumberFromString("A2");
-calibrationData.lat(1) = -47.15;
-calibrationData.vert(1) = -36.2;
+calibrationData.hole(1) = targetType.holeNumberFromString("Y4");
+calibrationData.lat(1) = -97.1;
+calibrationData.vert(1) = -3.9;
 
-calibrationData.hole(2) = targetType.holeNumberFromString("X22");
-calibrationData.lat(2) = -7.15;
-calibrationData.vert(2) = -6.2;
+calibrationData.hole(2) = targetType.holeNumberFromString("C31");
+calibrationData.lat(2) = -61.7;
+calibrationData.vert(2) = -36;
 
 calibration = Calibration(pvEngine);
 calibration.setCalibration(targetNumber, calibrationData);
@@ -27,17 +27,17 @@ calibration.setCalibration(targetNumber, calibrationData);
 % Target 2
 
 targetNumber = 2;
-targetType = Target_H15_C1_V11();
+targetType = Target_H15_C1_V10();
 
 calibrationData.targetTypeNumber = TargetDefinition.targetNumberByType(targetType);
 
-calibrationData.hole(1) = targetType.holeNumberFromString("A2");
-calibrationData.lat(1) = 47.15;
-calibrationData.vert(1) = 36.2;
+calibrationData.hole(1) = targetType.holeNumberFromString("C2");
+calibrationData.lat(1) = -48.85;
+calibrationData.vert(1) = -35.55;
 
-calibrationData.hole(2) = targetType.holeNumberFromString("X22");
-calibrationData.lat(2) = 7.15;
-calibrationData.vert(2) = 6.2;
+calibrationData.hole(2) = targetType.holeNumberFromString("Z33");
+calibrationData.lat(2) = -8.75;
+calibrationData.vert(2) = -0.1;
 
 calibration = Calibration(pvEngine);
 calibration.setCalibration(targetNumber, calibrationData);
@@ -52,13 +52,13 @@ targetType = Target_H15_C1_V11();
 
 calibrationData.targetTypeNumber = TargetDefinition.targetNumberByType(targetType);
 
-calibrationData.hole(1) = targetType.holeNumberFromString("A2");
-calibrationData.lat(1) = -147.15;
-calibrationData.vert(1) = -236.2;
+calibrationData.hole(1) = targetType.holeNumberFromString("X4");
+calibrationData.lat(1) = -77.484;
+calibrationData.vert(1) = -55.42;
 
-calibrationData.hole(2) = targetType.holeNumberFromString("F19");
-calibrationData.lat(2) = -227.15;
-calibrationData.vert(2) = -226.2;
+calibrationData.hole(2) = targetType.holeNumberFromString("B31");
+calibrationData.lat(2) = -41.784;
+calibrationData.vert(2) = -87.02;
 
 calibration = Calibration(pvEngine);
 calibration.setCalibration(targetNumber, calibrationData);
