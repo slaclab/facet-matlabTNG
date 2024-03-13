5fddbdc252a360fb1b3820906826c28f7760f6b4
SLAC fphysics <dstorey@slac.stanford.edu>
2024-03-12 18:10:14 -0700

    removed reference to aimes folder

--- F2_DAQ/read_BDES_table.m
+++ F2_DAQ/read_BDES_table.m
@@ -1,6 +1,6 @@
 function [BDES_list] = read_BDES_table()
 %Read BDES values from table for waist scan 
-cd '/home/fphysics/amth10/git_work/matlabTNG/F2_DAQ/'
+
 fileID = fopen('waist_BDES_values.txt','r');
 formatSpec = '%f %f %f %f %f %f %f %f %f';
 sizeA = [9 Inf];
