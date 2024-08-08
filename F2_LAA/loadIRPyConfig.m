b6a2c300b54934abcc8a42af7e07ab656f45c12f
SLAC fphysics <rariniel@slac.stanford.edu>
2024-08-08 13:37:59 -0700

    Fixed the sudden refusal of matlab to load python config files

--- F2_LAA/loadIRPyConfig.m
+++ F2_LAA/loadIRPyConfig.m
@@ -1,7 +1,11 @@
 function config = loadIRPyConfig()
 %LOADIRPYCONFIG Summary of this function goes here
 %   Detailed explanation goes here
-loadConfigForMatlab = py.importlib.import_module('loadConfigForMatlab');
-config = py2mat(loadConfigForMatlab.loadIRConfig());
+%loadConfigForMatlab = py.importlib.import_module('loadConfigForMatlab');
+config = py.loadConfigForMatlab.loadIRConfig();
+data = py.json.dumps(config);
+data = char(data);
+config = jsondecode(data);
+%config = py2mat(loadConfigForMatlab.loadIRConfig());
 end
 
--- F2_LAA/loadPyConfig.m
+++ F2_LAA/loadPyConfig.m
@@ -1,7 +1,11 @@
 function config = loadPyConfig()
 %LOADCONFIG Summary of this function goes here
 %   Detailed explanation goes here
-loadConfigForMatlab = py.importlib.import_module('loadConfigForMatlab');
-config = py2mat(loadConfigForMatlab.loadConfig());
+%loadConfigForMatlab = py.importlib.import_module('loadConfigForMatlab');
+config = py.loadConfigForMatlab.loadConfig();
+data = py.json.dumps(config);
+data = char(data);
+config = jsondecode(data);
+%config = py2mat(py.loadConfigForMatlab.loadConfig());
 end
 
