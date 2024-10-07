470734cd83bc3b9ed2943f40e3b5e32d11b1d62c
SLAC fphysics <rego@slac.stanford.edu>
2024-10-07 13:39:42 -0700

    fixed bugs related to DAQ compatibility with the code

--- F2_DAQ/scanFunc_LaserTime_S20Grating.m
+++ F2_DAQ/scanFunc_LaserTime_S20Grating.m
@@ -6,17 +6,23 @@ classdef scanFunc_LaserTime_S20Grating
        initial_readback
        daqhandle
        freerun = true
+       
+       initial_control_laser
+       initial_readback_laser
    end
-   properties(Constant) % [(grating property)  (laser property)]
-       control_PV = ["SIOC:SYS1:ML00CALCOUT070" "SIOC:SYS1:ML00CALCOUT071"]; %["XPS:LI20:MC03:M6" "OSC:LA20:10:FS_TGT_TIME"]
-       readback_PV = ["SIOC:SYS1:ML00CALCOUT070" "SIOC:SYS1:ML00CALCOUT071"];% ["XPS:LI20:MC03:M6.RBV" "OSC:LA20:10:FS_CTR_TIME"]
-       tolerance = [0.01 0.01]; % (!)
+   properties(Constant) 
+       control_PV = "SIOC:SYS1:ML00CALCOUT070"; %["XPS:LI20:MC03:M6" "OSC:LA20:10:FS_TGT_TIME"]
+       readback_PV = "SIOC:SYS1:ML00CALCOUT070";% ["XPS:LI20:MC03:M6.RBV" "OSC:LA20:10:FS_CTR_TIME"]
+       tolerance = 0.01; % (!)
+       
+       laser_PV_control = "SIOC:SYS1:ML00CALCOUT071";
+       laser_PV_readback = "SIOC:SYS1:ML00CALCOUT071";
+       laser_tolerance = 0.01;       
    end
     
    
    methods
        function obj = scanFunc_LaserTime_S20Grating(daqhandle)
-          
            if exist('daqhandle', 'var')
                obj.daqhandle=daqhandle;
                obj.freerun = false;
@@ -24,16 +30,19 @@ classdef scanFunc_LaserTime_S20Grating
            
            context = PV.Initialize(PVtype.EPICS_labca);
            obj.pvlist=[...
-               PV(context,'name',"control_S20Grating",'pvname',obj.control_PV(1),'mode',"rw",'monitor',true);% Control PV S20Grating
-               PV(context,'name',"control_LaserTime",'pvname',obj.control_PV(2),'mode',"rw",'monitor',true);% Control PV LaserTime
-               PV(context,'name',"readback_S20Grating",'pvname',obj.readback_PV(1),'mode',"r",'monitor',true);% Readback PV S20Grating
-               PV(context,'name',"readback_LaserTime",'pvname',obj.readback_PV(2),'mode',"r",'monitor',true);% Readback PV LaserTime
+               PV(context,'name',"control",'pvname',obj.control_PV,'mode',"rw",'monitor',true);% Control PV S20Grating
+               %PV(context,'name',"control_LaserTime",'pvname',obj.control_PV(2),'mode',"rw",'monitor',true);% Control PV LaserTime
+               PV(context,'name',"readback",'pvname',obj.readback_PV,'mode',"r",'monitor',true);% Readback PV S20Grating
+               %PV(context,'name',"readback_LaserTime",'pvname',obj.readback_PV(2),'mode',"r",'monitor',true);% Readback PV LaserTime
                ]; 
            pset(obj.pvlist,'debug',0);
            obj.pvs = struct(obj.pvlist);
            
-           obj.initial_control = [caget(obj.pvs.control_S20Grating) caget(obj.pvs.control_LaserTime)]; 
-           obj.initial_readback = [caget(obj.pvs.readback_S20Grating) caget(obj.pvs.readback_LaserTime)];
+           obj.initial_control = caget(obj.pvs.control); %[caget(obj.pvs.control_S20Grating) caget(obj.pvs.control_LaserTime)]; 
+           obj.initial_readback = caget(obj.pvs.readback); %[caget(obj.pvs.readback_S20Grating) caget(obj.pvs.readback_LaserTime)];
+           
+           obj.initial_control_laser = caget(laser_PV_control);
+           obj.initial_readback_laser = caget(laser_PV_readback);
        end
        
        function laser_time_val = laser_grating_calibration(s20_grating_val)
@@ -41,38 +50,34 @@ classdef scanFunc_LaserTime_S20Grating
            laser_time_val = caget(obj.pvs.readback_LaserTime) + s20_grating_val*slope;
        end
        
-       function delta = set_value(obj,value_grating, value_laser) 
-           arguments
-               obj scanFunc_LaserTime_S20Grating
-               value_grating double
-               value_laser double = laser_grating_calibration(value_grating)
-           end 
+       function delta = set_value(obj,value_grating)  
+           value_laser = laser_grating_calibration(value_grating);
+           
+           caput(obj.pvs.control,value_grating);
+           caput(obj.laser_PV_control,value_laser);
+           
+           obj.daqhandle.dispMessage(sprintf('Setting %s to %0.2f', obj.pvs.control.name, value_grating));
+           obj.daqhandle.dispMessage(sprintf('Setting %s to %0.2f', obj.laser_PV_control, value_laser));
+           
+           current_value = [caget(obj.pvs.readback) caget(obj.laser_PV_readback)];
            
            
-           caput(obj.pvs.control_S20Grating,value_grating);
-           caput(obj.pvs.control_LaserTime,value_laser);
-           
-           obj.daqhandle.dispMessage(sprintf('Setting %s to %0.2f', obj.pvs.control_S20Grating.name, value_grating));
-           obj.daqhandle.dispMessage(sprintf('Setting %s to %0.2f', obj.pvs.control_LaserTime.name, value_laser));
-           
-           current_value = [caget(obj.pvs.readback_S20Grating) caget(obj.pvs.readback_LaserTime)];
-           
-           
-           while max((current_value - [value_grating value_laser]) > obj.tolerance)
-               current_value(1) = caget(obj.pvs.readback_S20Grating);
-               current_value(2) = caget(obj.pvs.readback_LaserTime);
+           while max((current_value - [value_grating value_laser]) > [obj.tolerance obj.laser_tolerance])
+               current_value(1) = caget(obj.pvs.readback);
+               current_value(2) = caget(pbj.laser_PV_readback);
                pause(0.1);
            end
            
-           delta = current_value - [value_grating value_laser];
-           obj.daqhandle.dispMessage(springf('%s readback is %0.2f', obj.pvs.readback_S20Grating.name, current_value(1)));
-           obj.daqhandle.dispMessage(springf('%s readback is %0.2f', obj.pvs.readback_LaserTime.name, current_value(2)));
+           delta = current_value(1) - value_grating;
+           obj.daqhandle.dispMessage(springf('%s readback is %0.2f', obj.pvs.readback.name, current_value(1)));
+           obj.daqhandle.dispMessage(springf('%s readback is %0.2f', obj.laser_PV_readback, current_value(2)));
            
        end
        
        function restoreInitValue(obj)
            obj.daqhandle.dispMessage('Restoring initial value');
-           obj.set_value(obj.initial_control{:});
+           obj.set_value(obj.initial_control);
+           caput(obj.laser_PV_control,obj.initial_control_laser);
        end
     
    end
