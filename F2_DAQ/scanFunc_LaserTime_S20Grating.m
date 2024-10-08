6f5c11d6ab4f853ba68a6f81b42ad9746cb36062
SLAC fphysics <rego@slac.stanford.edu>
2024-10-08 09:49:47 -0700

    fixed issue with dummy variable

--- F2_DAQ/scanFunc_LaserTime_S20Grating.m
+++ F2_DAQ/scanFunc_LaserTime_S20Grating.m
@@ -7,17 +7,17 @@ classdef scanFunc_LaserTime_S20Grating
        daqhandle
        freerun = true
        
-       %initial_control_laser
-       %initial_readback_laser
+       initial_control_laser
+       initial_readback_laser
    end
    properties(Constant) 
-       control_PV = "SIOC:SYS1:ML00CALCOUT070" %["XPS:LI20:MC03:M6" "OSC:LA20:10:FS_TGT_TIME"]
-       readback_PV = "SIOC:SYS1:ML00CALCOUT070"% ["XPS:LI20:MC03:M6.RBV" "OSC:LA20:10:FS_CTR_TIME"]
+       control_PV = "SIOC:SYS1:ML00:CALCOUT070" %["XPS:LI20:MC03:M6" "OSC:LA20:10:FS_TGT_TIME"]
+       readback_PV = "SIOC:SYS1:ML00:CALCOUT070"% ["XPS:LI20:MC03:M6.RBV" "OSC:LA20:10:FS_CTR_TIME"]
        tolerance = 0.01; % (!)
        
-       %laser_PV_control = "SIOC:SYS1:ML00CALCOUT071"
-       %laser_PV_readback = "SIOC:SYS1:ML00CALCOUT071"
-       %laser_tolerance = 0.01;       
+       laser_PV_control = "SIOC:SYS1:ML00:CALCOUT071"
+       laser_PV_readback = "SIOC:SYS1:ML00:CALCOUT071"
+       laser_tolerance = 0.01;       
    end
    
    
@@ -41,45 +41,45 @@ classdef scanFunc_LaserTime_S20Grating
            obj.initial_control = caget(obj.pvs.control); %[caget(obj.pvs.control_S20Grating) caget(obj.pvs.control_LaserTime)]; 
            obj.initial_readback = caget(obj.pvs.readback); %[caget(obj.pvs.readback_S20Grating) caget(obj.pvs.readback_LaserTime)];
            
-           %obj.initial_control_laser = caget(laser_PV_control);
-           %obj.initial_readback_laser = caget(laser_PV_readback);
+           obj.initial_control_laser = caget(laser_PV_control);
+           obj.initial_readback_laser = caget(laser_PV_readback);
        end
        
-%        function laser_time_val = laser_grating_calibration(s20_grating_val)
-%            slope = 0.6*100*4000/16; % fs/mm
-%            laser_time_val = caget(obj.pvs.readback_LaserTime) + s20_grating_val*slope;
-%        end
+       function laser_time_val = laser_grating_calibration(s20_grating_val)
+           slope = 0.6*100*4000/16; % fs/mm
+           laser_time_val = caget(obj.pvs.readback_LaserTime) + s20_grating_val*slope;
+       end
        
        function delta = set_value(obj,value)  
-           %value_laser = laser_grating_calibration(value);
+           value_laser = laser_grating_calibration(value);
            
            caput(obj.pvs.control,value);
-           %caput(obj.laser_PV_control,value_laser);
+           caput(obj.laser_PV_control,value_laser);
            
            obj.daqhandle.dispMessage(sprintf('Setting %s to %0.2f', obj.pvs.control.name, value));
-           %obj.daqhandle.dispMessage(sprintf('Setting %s to %0.2f', obj.laser_PV_control, value_laser));
+           obj.daqhandle.dispMessage(sprintf('Setting %s to %0.2f', obj.laser_PV_control, value_laser));
            
-           %current_value = [caget(obj.pvs.readback) caget(obj.laser_PV_readback)];
-           current_value = caget(obj.pvs.readback);
+           current_value = [caget(obj.pvs.readback) caget(obj.laser_PV_readback)];
+           %current_value = caget(obj.pvs.readback);
            
            
-           while abs(current_value - value) > obj.tolerance
-               %while max((current_value - [value value_laser]) > [obj.tolerance obj.laser_tolerance])
-               current_value = caget(obj.pvs.readback);
-               %current_value(2) = caget(pbj.laser_PV_readback);
+           %while abs(current_value - value) > obj.tolerance
+           while max((current_value - [value value_laser]) > [obj.tolerance obj.laser_tolerance])
+               current_value(1) = caget(obj.pvs.readback);
+               current_value(2) = caget(pbj.laser_PV_readback);
                pause(0.1);
            end
            
-           delta = current_value - value;
-           obj.daqhandle.dispMessage(springf('%s readback is %0.2f', obj.pvs.readback.name, current_value));
-           %obj.daqhandle.dispMessage(springf('%s readback is %0.2f', obj.laser_PV_readback, current_value(2)));
+           delta = current_value(1) - value;
+           obj.daqhandle.dispMessage(springf('%s readback is %0.2f', obj.pvs.readback.name, current_value(1)));
+           obj.daqhandle.dispMessage(springf('%s readback is %0.2f', obj.laser_PV_readback, current_value(2)));
            
        end
        
        function restoreInitValue(obj)
            obj.daqhandle.dispMessage('Restoring initial value');
            obj.set_value(obj.initial_control);
-           %caput(obj.laser_PV_control,obj.initial_control_laser);
+           caput(obj.laser_PV_control,obj.initial_control_laser);
        end
     
    end
